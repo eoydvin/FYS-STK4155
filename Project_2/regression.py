8ce02f4e82d181defc8ca3a660db725fb2b28674
erlnwind <erlend.oydvin@gmail.com>
2021-10-22 12:31:20 +0200

    more

--- Project_2/regression.py
+++ Project_2/regression.py
@@ -10,18 +10,42 @@ import numpy as np
 
 class OLS(object):
     """
-    Fits a polynomial to given data using OLS.         
+    Fits a polynomial to given data using OLS. Uses stcastic gradient descent.    
+
     """
-    def __init__(self, x, y, z, p, design='orig'):
-        """
-        Sets up design matrix for a k-order polynomial. 
+    def __init__(self, p, learning_rate, batch_size=256, max_iter=100):
+        """        
+        Fit polynomial using SGD. 
         
-        :param x: x value for given z=f(x,y)
-        :param y: y value for given z=f(x,y)
-        :param z: z value for given z=f(x,y) 
-        :param p: order p of polynomial
+        Parameters
+        ----------
+        p : order of polynomial
+        learning_rate : learning rate
+        batch_size : batch_size of SDG
+        max_iter : maximum number of iterations SGD
+        
         """
         
+        self.p = p
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.max_iter = max_iter
+        
+    def fit(x, z):
+        """
+        Fit polynomial using SGD. 
+        
+        Parameters
+        ----------
+        x : Matrix of features and samples
+        z : Array of corresponding realizations
+  
+
+        Returns
+        -------
+        None.
+
+        """
         # Construct design matrix
         n = len(x)
         M = np.zeros([n, int((p+1)*(p+2)/2)])
@@ -30,15 +54,10 @@ class OLS(object):
             for i in range(0, p+1-j): # x **i
                 M[:, c] = (x**i)*(y**j)
                 c += 1
-
-        if design == 'skl': # test to study differences  with skl
-            n = len(x)
-            M = np.zeros([n, int((p+1)*(p+2)/2)])
-            c = 0
-            for i in range(0, p+1): # y**j
-                for j in range(0, i+1): # x **i
-                    M[:, c] = (x**(i - j))*(y**j)
-                    c += 1
+        # SGD:
+            #del inn i b batcher,
+            #beregn b for en batch
+            #send beta videre til neste batch
 
         self.beta_OLS = np.linalg.pinv(M.T @ M) @ M.T @ z.reshape([n, 1])
         
@@ -108,16 +127,6 @@ class OLS(object):
             for i in range(0, self.p+1 - j): # x **i
                 M[:, c] = (x**i)*(y**j)
                 c += 1
-
-        if self.design == 'skl': # test to study differences  with skl
-            n = len(x)
-            M = np.zeros([n, int((self.p+1)*(self.p+2)/2)])
-            c = 0
-            for i in range(0, self.p+1): # y**j
-                for j in range(0, i+1): # x **i
-                    M[:, c] = (x**(i - j))*(y**j)
-                    c += 1
-
        
         z_pred = M @ self.beta_OLS    
         
@@ -146,15 +155,6 @@ class OLS(object):
             for i in range(0, self.p+1 - j): # x **i
                 M[:, c] = (x**i)*(y**j)
                 c += 1
-        
-        if self.design == 'skl': # test to study differences  with skl
-            n = len(x)
-            M = np.zeros([n, int((self.p+1)*(self.p+2)/2)])
-            c = 0
-            for i in range(0, self.p+1): # y**j
-                for j in range(0, i+1): # x **i
-                    M[:, c] = (x**(i - j))*(y**j)
-                    c += 1
 
         
         z_pred = M @ self.beta_OLS
@@ -163,129 +163,3 @@ class OLS(object):
         # Test ved bruk av M fra init gir samme resultat
         return np.diagonal(variance_z*np.linalg.pinv(M.T @ M))  
 
-class Ridge(object):
-    """
-    Fits a polynomial to given data using OLS.         
-    """
-    def __init__(self, x, y, z, p, lam):
-        """
-        Sets up design matrix for a k-order polynomial. 
-        
-        :param x: x value for given z=f(x,y)
-        :param y: y value for given z=f(x,y)
-        :param z: z value for given z=f(x,y) 
-        :param p: order p of polynomial
-        """
-        
-        # Construct design matrix
-        n = len(x)
-        M = np.zeros([n, int((p+1)*(p+2)/2)])
-        c = 0
-        for j in range(0, p+1): # y**j
-            for i in range(0, p+1 - j): # x **i
-                M[:, c] = (x**i)*(y**j)
-                c += 1
-        
-        self.beta_Ridge = np.linalg.pinv(
-            M.T @ M + np.eye(M.shape[1])*lam) @ M.T @ z.reshape([n, 1])
-        self.p = p
-        self.lam = lam
-    
-    def R2(self, x, y, z):
-        """
-        Calculate R2 using 
-        
-        R2 = 1 - (sum_i ((z_pred_i - z_i)**2)) / (sum_i ((z_i_mean - z_i)**2))
-
-        :param x: array of x coordinates
-        :param y: array of y coordinates
-        :param z: array of known z values      
-
-        """
-        # Construct design matrix
-        n = len(x)
-        M = np.zeros([n, int((self.p+1)*(self.p+2)/2)])
-        c = 0
-        for j in range(0, self.p+1): # y**j
-            for i in range(0, self.p+1-j): # x **i
-                M[:, c] = (x**i)*(y**j)
-                c += 1
-        
-        z_pred = M @ self.beta_Ridge    
-        return 1 - np.sum((z_pred - z.reshape([n, 1]))**2)/np.sum(
-            (np.mean(z) - z.reshape([n, 1]))**2)    
-                
-    def predict(self, x, y):
-        """
-        Predict f(x, y) = z for given x and y
-        
-        :param x: array of x coordinates
-        :param y: array of y coordinates
-        
-        :return: array of predicted z values
-        """
-        # Construct design matrix
-        n = len(x)
-        M = np.zeros([n, int((self.p+1)*(self.p+2)/2)])
-        c = 0
-        for j in range(0, self.p+1): # y**j
-            for i in range(0, self.p+1-j): # x **i
-                M[:, c] = (x**i)*(y**j)
-                c += 1
-        return M @ self.beta_Ridge
-    
-    def MSE(self, x, y, z):
-        """
-        Calculate MSE using 
-        
-        MSE = 1/n * sum_i ((z_pred_i - z_i)**2)
-
-        :param x: array of x coordinates
-        :param y: array of y coordinates
-        :param z: array of known z values      
-
-        """
-        # Construct design matrix
-        n = len(x)
-        M = np.zeros([n, int((self.p+1)*(self.p+2)/2)])
-        c = 0
-        for j in range(0, self.p+1): # y**j
-            for i in range(0, self.p+1-j): # x **i
-                M[:, c] = (x**i)*(y**j)
-                c += 1
-        
-        z_pred = M @ self.beta_Ridge    
-        
-        return np.mean((z_pred - z.reshape([n, 1]))**2)
-        
-    
-    def var_beta(self, x, y, z):
-        """
-        Calculate variance in beta by comparing predicted values with test 
-        values according to Hastie et al:
-        
-        var(beta) = inv(X.T X) * var(y), where:    
-        var(y) = 1 / (N - p - 1) * sum((z_i  - z_pred_i)**2)
-        
-        :param x: array of x coordinates
-        :param y: array of y coordinates
-        :param z: array of known z values
-        
-        :return: array of estiamted variance for beta values
-        """
-        # Construct design matrix
-        n = len(x)
-        M = np.zeros([n, int((self.p+1)*(self.p+2)/2)])
-        c = 0
-        for j in range(0, self.p+1): # y**j
-            for i in range(0, self.p+1 - j): # x **i
-                M[:, c] = (x**i)*(y**j)
-                c += 1
-        
-        z_pred = M @ self.beta_Ridge
-        variance_z = (1/(n - self.p - 1))*np.sum((
-            z_pred - z.reshape([n, 1]))**2)
-        # Test ved bruk av M fra init gir samme resultat
-        return np.diagonal(variance_z*np.linalg.pinv(
-            M.T @ M + np.eye(M.shape[1])*self.lam) @ M.T @ M @ (np.linalg.pinv(M.T @ M + np.eye(M.shape[1])*self.lam)).T)  
-    

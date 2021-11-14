f827b31622b6243f4ac6a9b848d33626b359df2f
Erlend Øydvin <erlend.oydvin@gmail.com>
2021-11-14 22:00:25 +0100

    more

--- Project_2/neural_network.py
+++ Project_2/neural_network.py
@@ -6,7 +6,10 @@ Created on Sun Oct 31 10:17:33 2021
 @author: erlend
 """
 
-import numpy as np
+import autograd.numpy as np
+from autograd import elementwise_grad as egrad 
+import sys
+
 
 class NeuralNetwork:
     def __init__(
@@ -15,7 +18,8 @@ class NeuralNetwork:
             Y_data,
             n_categories,
             output_func,
-            hidden_layer_sizes=[50], #list of number of neurons in each layer, last is output
+            activation_func = 'logistic',
+            hidden_layer_sizes=[50, 10],
             epochs=10,
             batch_size=100,
             eta=0.1,
@@ -27,7 +31,7 @@ class NeuralNetwork:
         self.n_inputs = X_data.shape[0]
         self.n_features = X_data.shape[1]
         self.n_categories = n_categories
-        self.hidden_layer_sizes = hidden_layer_sizes #nodes in layers
+        self.hidden_layer_sizes = hidden_layer_sizes 
 
         self.epochs = epochs
         self.batch_size = batch_size
@@ -35,17 +39,23 @@ class NeuralNetwork:
         self.eta = eta
         self.lmbd = lmbd
         
-        #Neurons in input layer
-        self.weights = [np.random.randn(self.n_features, self.hidden_layer_sizes[0])] 
+        #Initialize weigths and biases
+        # - w: normal distribution
+        # - b: sligthly more than zero
+        #Input layer
+        self.weights = [np.random.randn(
+            self.n_features, self.hidden_layer_sizes[0])] 
         self.bias = [np.zeros(self.hidden_layer_sizes[0]) + 0.01]
         
-        #Neurons in hidden layers
+        #Hidden layers
         for new_neurons in self.hidden_layer_sizes[1:]:
-            self.weights.append(np.random.randn(self.weights[-1].shape[1], new_neurons))
+            self.weights.append(
+                np.random.randn(self.weights[-1].shape[1], new_neurons))
             self.bias.append(np.zeros(new_neurons) + 0.01)
-       
+        
         #output layer
-        self.weights.append(np.random.randn(self.weights[-1].shape[1], self.n_categories))
+        self.weights.append(np.zeros(
+            [self.weights[-1].shape[1], self.n_categories]))
         self.bias.append(np.zeros(self.n_categories) + 0.01)
         
         #scale weights so they match x-input..
@@ -54,36 +64,64 @@ class NeuralNetwork:
 
         self.a_h = [i for i in range(len(self.hidden_layer_sizes) + 1)] #+1 is outputlayer
         #TODO
-        #Implement general f', derivative in backpropagation
-        #List of activation functions
+        # Implement general f', derivative in backpropagation
+        # List of activation functions
+        
+        if activation_func == 'logistic':
+            from neural_network import logistic
+            from neural_network import logistic_derivative
+            self.activation_func = logistic
+            self.d_activation_func = logistic_derivative
+        
+        elif activation_func == 'relu':
+            from neural_network import relu
+            from neural_network import relu_derivative
 
-    def sigmoid(self, x):
-        #return np.tanh(x)
-        return 1/(1 + np.exp(-x))
+            self.activation_func = relu
+            self.d_activation_func = relu_derivative
 
+        elif activation_func == 'leakyrelu':
+            from neural_network import leakyrelu
+            self.activation_func = leakyrelu
+            self.d_activation_func = egrad(self.activation_func)
+    
+    def _logistic(self, x):
+        return 1 / (1 + np.exp(-x)) 
+
+            
     def feed_forward(self):
     
         # feed-forward for training
-         
-        self.a_h[0] = self.sigmoid(np.matmul(self.X_data, self.weights[0]) + self.bias[0])
+        self.a_h[0] = self.activation_func( 
+            np.matmul(self.X_data, self.weights[0]) + self.bias[0])
         for l in range(1, len(self.hidden_layer_sizes)):
-            self.a_h[l] = self.sigmoid(np.matmul(self.a_h[l - 1], self.weights[l])+self.bias[l])
+            self.a_h[l] = self.activation_func( 
+                np.matmul(self.a_h[l - 1], self.weights[l])+self.bias[l])
         
-        self.a_h[-1] = self.output_func(np.matmul(self.a_h[-2], self.weights[-1]) + self.bias[-1])
+        self.a_h[-1] = self.output_func(
+            np.matmul(self.a_h[-2], self.weights[-1]) + self.bias[-1])
 
     def backpropagation(self):
         error = self.a_h[-1] - self.Y_data #delta output
+       
         for l in range(len(self.hidden_layer_sizes), 0, -1):
             weights_gradient = np.matmul(self.a_h[l - 1].T, error)
-            bias_gradient = np.sum(error, axis=0) #kan ve fler kolonner
+            bias_gradient = np.sum(error, axis=0) 
+
 
             if self.lmbd > 0.0:
                 weights_gradient += self.lmbd * self.weights[l]
 
             self.weights[l] -= self.eta * weights_gradient
             self.bias[l] -= self.eta * bias_gradient
-
-            error = np.matmul(error, self.weights[l].T)*self.a_h[l-1]*(1 - self.a_h[l-1])
+            
+            
+            #error = np.matmul(
+            #    error, self.weights[l].T) *self.a_h[l-1]*(1 - self.a_h[l-1])
+            
+            error = np.matmul(
+                error, self.weights[l].T)* self.d_activation_func( 
+                        np.matmul(self.a_h[l - 1], self.weights[l]) + self.bias[l]) 
 
         weights_gradient = np.matmul(self.X_data.T, error)
         bias_gradient = np.sum(error, axis=0)
@@ -91,16 +129,20 @@ class NeuralNetwork:
         if self.lmbd > 0.0:
             weights_gradient += self.lmbd * self.weights[0]
 
+        
         self.weights[0] -= self.eta * weights_gradient
         self.bias[0] -= self.eta * bias_gradient
 
 
     def feed_forward_out(self, X):
-        self.a_h[0] = self.sigmoid(np.matmul(X, self.weights[0]) + self.bias[0])
+        self.a_h[0] = self.activation_func(np.matmul( 
+            X, self.weights[0]) + self.bias[0])
         for l in range(1, len(self.hidden_layer_sizes)):
-            self.a_h[l] = self.sigmoid(np.matmul(self.a_h[l - 1], self.weights[l])+self.bias[l])
+            self.a_h[l] = self.activation_func(  
+                np.matmul(self.a_h[l - 1], self.weights[l])+self.bias[l])
         
-        self.a_h[-1] = self.output_func(np.matmul(self.a_h[-2], self.weights[-1]) + self.bias[-1])
+        self.a_h[-1] = self.output_func(
+            np.matmul(self.a_h[-2], self.weights[-1]) + self.bias[-1])
         return self.a_h[-1]
 
 
@@ -112,7 +154,7 @@ class NeuralNetwork:
 
         for i in range(self.epochs):
             for j in range(self.iterations):
-                # pick datapoints with replacement
+                # pick datapoints with no replacement
                 chosen_datapoints = np.random.choice(
                     data_indices, size=self.batch_size, replace=False
                 )
@@ -120,7 +162,22 @@ class NeuralNetwork:
                 # minibatch training data
                 self.X_data = self.X_data_full[chosen_datapoints]
                 self.Y_data = self.Y_data_full[chosen_datapoints]
-
                 self.feed_forward()
                 self.backpropagation()
-            print("Epoch: ", i)
+
+
+def logistic(x):
+    return 1 / (1 + np.exp(-x))
+
+def logistic_derivative(x):
+    return logistic(x) * (1.0 - logistic(x))
+
+def relu(x):
+    np.clip(x, a_min = 0.0, a_max = sys.float_info.max, out = x)
+    return x
+
+def relu_derivative(self, x) :
+    return relu(np.heaviside(x, 0.0) )
+
+def leakyrelu(self, x) :
+    return (x >= 0.0) * x + (x < 0.0) * (self.alpha * x)
--- Project_2/regression.py
+++ Project_2/regression.py
@@ -62,33 +62,26 @@ class OLS(object):
 
         """
         M = self.p_design(X)
-        m = int(M.shape[0]/self.batch_size) # number minibatches       
+        z = z.reshape(-1, 1) #to be sure
+        iterations = int(M.shape[0]/self.batch_size) # number minibatches       
         
         w = np.zeros(M.shape[1]).reshape(-1, 1) 
         v = np.zeros(M.shape[1]).reshape(-1, 1) 
-        # maybe use random numbers that us prop. to eigenvalues? 
         
         def c(beta, ytrue, x_design):
-            return (1/ytrue.size)*(ytrue - x_design @ beta).T @ (ytrue - x_design @ beta) + self.lam * beta.T @ beta
+            return (1/ytrue.size)*(ytrue - x_design @ beta).T @ (
+                ytrue - x_design @ beta) + self.lam * beta.T @ beta
         g = grad(c, 0)
         
-        #test med å lage grad i loopen, kanskje det har med matriser å gjhere dette. 
-        
+        data_indices = np.arange(M.shape[0]) #list of datanumbers
         for epoch in range(self.n_epochs):
-            # lag batcher her:) 
-            Xz = np.hstack([M, z])
-            np.random.shuffle(Xz) #shuffle
-            Xz_split = np.array_split(Xz, m) #split into batches
-            
-            for batch in Xz_split:
-        
-                #gr = (1.0/m) *batch[:, 0:-1].T @ ((batch[:, 0:-1] @ w) - batch[:, -1].reshape(-1, 1))
-                gr = g(w, batch[:, -1].reshape(-1, 1), batch[:, 0:-1])
-                #if not np.isnan( np.sum(g) ):
-  
+            for j in range(iterations):
+                # pick datapoints with no replacement
+                datapoints = np.random.choice(
+                        data_indices, size=self.batch_size, replace=False
+                        )
+                gr = g(w, z[datapoints], M[datapoints, :])
                 v = self.alpha*v - self.learning_rate* gr
-                #else:
-                #    v = 0
                 w = w + v
 
         self.w = w
@@ -142,129 +135,3 @@ class OLS(object):
     
     
 
-class Ridge(OLS):
-    """
-    Fits a polynomial to given data using OLS. Uses stcastic gradient descent.    
-
-    """
-    def __init__(self, p, lam, learning_rate, alpha, batch_size=32, 
-                 max_iter=100, n_epochs=50):
-        """        
-        Fit polynomial using SGD. 
-        
-        Parameters
-        ----------
-        p : order of polynomial
-        learning_rate : learning rate
-        batch_size : batch_size of SDG
-        max_iter : maximum number of iterations SGD
-        
-        """
-        
-        self.p = p
-        self.batch_size = batch_size
-        self.max_iter = max_iter
-        self.n_epochs = n_epochs
-        self.learning_rate = learning_rate
-        self.alpha = alpha
-        self.lam = lam
-    
-    def p_design(self, X):
-        """
-        Construct design matrix for a polynomial fit. 
-
-        """
-        M = np.zeros([X.shape[0], int((self.p+1)*(self.p+2)/2)])
-        c = 0
-        for j in range(0, self.p+1): # y**j
-            for i in range(0, self.p+1-j): # x **i
-                M[:, c] = (X[:, 0]**i)*(X[:, 1]**j)
-                c += 1
-        return M
-            
-        
-    def fit(self, X, z):
-        """
-        Fit polynomial using SGD. 
-        
-        Parameters
-        ----------
-        X : Matrix of features and samples
-        z : Array of corresponding realizations
-
-        """
-        M = self.p_design(X)
-        m = int(M.shape[0]/self.batch_size) # number minibatches       
-        
-        w = np.zeros(M.shape[1]).reshape(-1, 1) 
-        v = np.zeros(M.shape[1]).reshape(-1, 1) 
-        # maybe use random numbers that us prop. to eigenvalues? 
-
-        for epoch in range(self.n_epochs):
-            # lag batcher her:) 
-            Xz = np.hstack([M, z])
-            np.random.shuffle(Xz) #shuffle
-            Xz_split = np.array_split(Xz, m) #split into batches
-            
-            for batch in Xz_split:
-                g = (1.0/m) *batch[:, 0:-1].T @ ((batch[:, 0:-1] @ w) - batch[:, -1].reshape(-1, 1))
-                if not np.isnan( np.sum(g) ):
-                    v = self.alpha*v - self.learning_rate*g
-                else:
-                    v = 0
-                w = w + v
-
-        self.w = w
-    
-    def predict(self, X):
-        """
-        Predict f(x, y) = z for given X[x, y] a
-        
-        Parameters
-        ----------
-        X : Matrix of features and samples
-        
-        :return: array of predicted z values
-        """
-        M = self.p_design(X)
-        return M @ self.w
-    
-    def MSE(self, X, z):
-        """
-        Calculate MSE using 
-        
-        MSE = 1/n * sum_i ((z_pred_i - z_i)**2)
-
-        :param x: array of x coordinates
-        :param y: array of y coordinates
-        :param z: array of known z values      
-
-        """
-        M = self.p_design(X)    
-        z_pred = M @ self.w   
-        
-        #print(self.w)
-        #print(z_pred.shape)
-        return np.mean((z_pred - z.reshape([M.shape[0], 1]))**2) 
-        
-    def R2(self, X, z):
-        """
-        Calculate R2 using 
-        
-        R2 = 1 - (sum_i ((z_pred_i - z_i)**2)) / (sum_i ((z_i_mean - z_i)**2))
-
-        :param x: array of x coordinates
-        :param y: array of y coordinates
-        :param z: array of known z values      
-
-        """
-        M = self.p_design(X)    
-        z_pred = M @ self.w   
-        return 1 - np.sum(( z.reshape(-1, 1) - z_pred.reshape(-1, 1) )**2)/np.sum(
-            ( z.reshape(-1, 1) - np.mean(z))**2)
-    
-    
-    
-    
-    
-    
